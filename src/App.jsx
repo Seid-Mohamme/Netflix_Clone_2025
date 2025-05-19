6dc5b241ade2549a9aee083aa67afed276abcc81
Seid-Mohamme <seidmamme@gmail.com>
2025-05-19 17:03:19 +0300

    last editing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import React from "react";
 import "./App.css";
-// import "./Components/Header/Header.css";
 import Home from "./Pages/Home/Home";
-// import Header from "../src/Components/Header/Header";
-// import Footer from "../src/Components/Footer/Footer";
 import "./Components/Header/header.module.css";
 
 function App() {
